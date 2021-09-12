72e1594f01e040e0ffa8efdd493c8371b7c50fa2
Jamie Williams <jamwil1226@yahoo.com>
2021-09-12 18:59:13 -0400

    last commit for submitting challenge

--- script.js
+++ script.js
@@ -1,85 +1,85 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// RANDOM PASSWORD GENERATOR
 
-
-// Prompt user to choose character length 8 - 128
-var confirmLength = window.prompt("How many characters would you like your password to be? Must be between 8 - 128."); {
-  console.log(confirmLength);
-}
-
-// Confirm lowercase letters in password 
-var confirmLowercase = window.confirm("Click OK to confirm including lowercase characters."); {
-  console.log(confirmLowercase);
-}
-
-// Confirm uppercase letters in password 
-var confirmUppercase = window.confirm("Click OK to confirm including uppercase characters."); {
-  console.log(confirmUppercase);
-}
-
-// Confirm numbers in password 
-var confirmNumbers = window.confirm("Click OK to confirm including numeric characters."); {
-  console.log(confirmNumbers);
-}
-
-// Confirm symbols in password 
-var confirmSymbols = window.confirm("Click OK to confirm including special characters."); {
-  console.log(confirmSymbols);
-}
-
-// Generate random password that matches selected criteria
-
-var randomPassword = {
-  length: confirmLength,
-  lower: confirmLowercase,
-  upper: confirmUppercase, 
-  numbers: confirmNumbers, 
-  symbols: confirmSymbols
-};
-
-console.log('randomPassword: ', randomPassword)
-
-function getRandomLength() {
-  var length = confirmLength
-  return confirmLength;
-}
-console.log(getRandomLength());
-
-function getRandomLowercase() {
-  var lowercase = 'abcdefghijklmnopqrstuvwxyz'
-  return lowercase[Math.floor(Math.random() * lowercase.length)];
-}
-console.log(getRandomLowercase());
-
-function getRandomUppercase() {
-  var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  return uppercase[Math.floor(Math.random() * uppercase.length)];
-}
-console.log(getRandomUppercase());
-
-function getRandomNumbers() {
-  var numbers = '1234567890'
-  return numbers[Math.floor(Math.random() * numbers.length)];
-}
-console.log(getRandomNumbers());
-
-function getRandomSymbols() {
-  var symbols = '!@#$%^&*()_+{}[]:<>?/'
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-console.log(getRandomSymbols());
-
-
-
-// Generate password function
+// Generate password
 function generatePassword() {
-  const userInputArray = [ { confirmLowercase } , { confirmUppercase }, { confirmNumbers }, { confirmSymbols }].filter
-  (
-    item => Object.values(item)[0]
-  );
-  console.log('userInputArray: ', userInputArray);
+  
+  // Prompt user to choose character length 8 - 128
+  var confirmLength = window.prompt("How many characters would you like your password to be? Must be between 8 - 128."); 
+    console.log(confirmLength);
+
+  while(confirmLength < 8 || confirmLength > 128){
+    alert("Please enter a number between 8 - 128.")
+    confirmLength = prompt("How many characters would you like your password to be? Must be between 8 - 128.");
+  }
+  alert("Thank you. Your password will be " + confirmLength + " characters in length.");
+  
+  // Confirm lowercase letters in password 
+  var confirmLowercase = window.confirm("Click OK to confirm including lowercase characters."); 
+    console.log(confirmLowercase);
+
+  if (confirmLowercase) {
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    alert("Your password will contain lowercase letters.");
+  }
+  else {
+    var lowercase = ""
+    alert("Your password will not contain lowercase letters.");
+  };
+
+  // Confirm uppercase letters in password 
+  var confirmUppercase = window.confirm("Click OK to confirm including uppercase characters."); 
+    console.log(confirmUppercase);
+
+  if (confirmUppercase === true) {
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+    alert("Your password will contain uppercase letters.");
+  }
+  else {
+    var uppercase = ""
+    alert("Your password will not contain uppercase letters.");
+  };
+
+  // Confirm numbers in password 
+  var confirmNumbers = window.confirm("Click OK to confirm including numeric characters."); 
+    console.log(confirmNumbers);
+
+  if (confirmNumbers) {
+    var numbers = '1234567890';
+    alert("Your password will contain numbers.");
+  }
+  else {
+    var numbers = ""
+    alert("Your password will not contain numbers.");
+  };
+
+  // Confirm symbols in password 
+  var confirmSymbols = window.confirm("Click OK to confirm including special characters."); 
+    console.log(confirmSymbols);
+
+  if (confirmSymbols) {
+    var symbols = '!@#$%^&*()_+{}[]:<>?/';
+    alert("Your password will contain symbols.");
+  }
+  else {
+    var symbols = ""
+    alert("Your password will not contain symbols.");
+  };
+
+  // calculation of user input
+  var userInput = [lowercase, uppercase, numbers, symbols];
+  console.log("user input: ", userInput);
+
+  var generatedPassword = "";
+
+  for (var i = 0; i < confirmLength; i++) {
+  generatedPassword = userInput(Math.floor(Math.random() * confirmLength));
+  }
+
+  return generatedPassword();
 }
 
+// generate button
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -91,6 +91,4 @@ function writePassword() {
 } 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
-  
-
+generateBtn.addEventListener("click", writePassword);
